258d64ab94646c5788005764e3358fda2a4d5d39
Shang Ziyuan <happy.red.bear@gmail.com>
2026-05-15 23:38:12 +0800

    add version, magic number, filetype check

--- file_parser.c
+++ file_parser.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(void)
 {
@@ -17,6 +18,27 @@ int main(void)
     }
 
     fread(buffer, sizeof(buffer), 1, ptr);
+
+    // check version
+    uint32_t version;
+    memcpy(&version, &buffer[0], 4);
+    printf("Version: %u\n", version);  // little-endian 32-bit integer
+
+    // check magic number
+    if (memcmp(&buffer[0x04], "relogic", 7) != 0) {
+        fprintf(stderr, "invalid file format\n");
+        fclose(ptr);
+        return 1;
+    }
+
+    // check file type
+    unsigned char wldFile = 2;
+    
+    if (memcmp(&buffer[0x0B], &wldFile, 1) == 0) {
+        printf("This is a world file\n");
+    } else {
+        printf("This is not a world file\n");
+    }
 
     for(int i = 0; i<20; i++)
         if (buffer[i] >= 32 && buffer[i] <= 126)  // within ASCII range
